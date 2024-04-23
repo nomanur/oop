650c021d685533c147c77abce768c033113a4ffc
Nomanur Rahman <nomanurrahman@gmail.com>
2024-04-23 19:34:21 +0600

    oop basic

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+// --cllass
+// --property
+// -method
+// -objection
+
+class Test {  //class
+
+    public $testProp;    //property
+    private $privateProp;
+    protected $protectedProp;
+
+    public function testFunction() {  //method
+        echo "test function";
+    }
+
+}
+
+$obj = new Test(); //object
+
+$obj->testFunction(); //call object function
